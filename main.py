d5a82027643989c2c0fe00444ebe87448a6070bd
gdula3000 <xpiotrrx@gmail.com>
2023-11-05 15:13:20 +0100

    .

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import random
+
+
+def random_number(x):
+    y = random.randint(1, 7)
+    print(y)
+    if x == y:
+        return True
+    else:
+        return False
+
+
+print(random_number(5))
